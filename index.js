3474432e9c0b98e3d69c5c4f85e453b2ae4e0f6c
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-14 21:55:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,19 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase() === string){
+  if(string.toLowerCase() === string) {
     return "I can't hear you!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if(string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if(string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
